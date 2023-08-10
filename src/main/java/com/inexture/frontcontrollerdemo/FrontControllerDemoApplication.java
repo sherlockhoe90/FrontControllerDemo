df8dfc5afc6915a24c85a41721327019682a387f
sherlex <hadespinoy@gmail.com>
2023-08-10 16:06:33 +0530

    Got rid of Spring Boot and used just Spring Core and Spring Web (webmvc).

--- src/main/java/com/inexture/frontcontrollerdemo/FrontControllerDemoApplication.java
+++ src/main/java/com/inexture/frontcontrollerdemo/FrontControllerDemoApplication.java
@@ -1,12 +1,13 @@
 package com.inexture.frontcontrollerdemo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FrontControllerDemoApplication {
 
-    @GetMapping("/person")
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
     public String sayHello() {
         String greeting = "Hello World";
         System.out.println(greeting);
